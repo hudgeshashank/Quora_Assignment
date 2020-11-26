b4add9f5b06b48f29aba87a83b4c63b90b487b1a
ChethanShetty563 <chethan563@gmail.com>
2020-11-27 01:53:30 +0530

    Added the more methids to fetch data based on the parameters, logic will be handled by teammates

--- quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
+++ quora-service/src/main/java/com/upgrad/quora/service/dao/UserDao.java
@@ -1,5 +1,6 @@
 package com.upgrad.quora.service.dao;
 
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import org.springframework.stereotype.Repository;
 
@@ -15,6 +16,35 @@ public class UserDao {
     public UserEntity createUser(UserEntity userEntity)
     {
 //        Add the logic to persist the signup user data
+        return null;
+    }
+    public UserEntity getUserByUserName(final String username)
+    {
+//        Write the logic to fetch user by username
+            return null;
+    }
+    public UserEntity getUserByEmail(final String email)
+    {
+//        Write the logic to fetch user by email
+        return null;
+
+    }
+
+    public UserAuthTokenEntity getAuthToken(final String authorization)
+    {
+//        write logic to fetch access token
+     return null;
+    }
+
+    public UserAuthTokenEntity saveLogin(final UserAuthTokenEntity userAuthTokenEntity)
+    {
+//        perssit the login data
+        return null;
+    }
+
+    public UserAuthTokenEntity signout(final UserAuthTokenEntity userAuthTokenEntity)
+    {
+//        Write the logic to update sign out data
         return null;
     }
 
