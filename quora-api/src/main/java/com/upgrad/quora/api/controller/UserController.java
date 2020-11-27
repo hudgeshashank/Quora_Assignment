32a590b61789b8b4546308b9f1c5dad0d782f35d
Chirag-debug <mulla.chirag@gmail.com>
2020-11-27 21:38:39 +0530

    user/signup endpoint - controller, service, dao, Rest E H

--- quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
+++ quora-api/src/main/java/com/upgrad/quora/api/controller/UserController.java
@@ -7,16 +7,20 @@ import com.upgrad.quora.api.model.SignupUserRequest;
 import com.upgrad.quora.api.model.SignupUserResponse;
 import com.upgrad.quora.service.business.AuthenticationService;
 import com.upgrad.quora.service.business.UserBusinessService;
+import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.SignOutRestrictedException;
 import com.upgrad.quora.service.exception.SignUpRestrictedException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping
@@ -31,7 +35,25 @@ public class UserController {
     @RequestMapping(method = RequestMethod.POST, path = "/user/signup", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<SignupUserResponse> signup(final SignupUserRequest signupUserRequest) throws SignUpRestrictedException {
 //     Write the Controller to sign up and send the data to userbusiness service
-        return null;
+
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUuid(UUID.randomUUID().toString());
+        userEntity.setFirstName(signupUserRequest.getFirstName());
+        userEntity.setLastName(signupUserRequest.getLastName());
+        userEntity.setEmail(signupUserRequest.getEmailAddress());
+        userEntity.setUserName(signupUserRequest.getUserName());
+        userEntity.setPassword(signupUserRequest.getPassword());
+        userEntity.setDob(signupUserRequest.getDob());
+        userEntity.setCountry(signupUserRequest.getCountry());
+        userEntity.setAboutMe(signupUserRequest.getAboutMe());
+        userEntity.setRole("nonadmin"); // confirm
+        userEntity.setContactNumber(signupUserRequest.getContactNumber());
+
+        final UserEntity createdUserEntity = userBusinessService.signup(userEntity);
+        final SignupUserResponse userResponse = new SignupUserResponse().id(createdUserEntity.getUuid())
+                .status("USER SUCCESSFULLY REGISTERED");
+
+        return new ResponseEntity<SignupUserResponse>(userResponse,HttpStatus.CREATED);
     }
 
     @RequestMapping(method = RequestMethod.POST, path = "/user/signin", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -21,9 +21,33 @@ public class UserBusinessService {
 
     @Transactional(propagation = Propagation.REQUIRED)
     public UserEntity signup(UserEntity userEntity) throws SignUpRestrictedException {
-//        Write the logic to sign up the user
-//        If the username already exist throw exception given in problem statement
-//        if the email already exists throw exception given in prolem statement
-        return null;
+        //        Write the logic to sign up the user
+        //        If the username already exist throw exception given in problem statement
+        //        if the email already exists throw exception given in prolem statement
+
+        String userNameInEntity = userEntity.getUserName(); // not persisted data
+        UserEntity userCheck = userDao.getUserByUserName(userNameInEntity);// data from database
+
+        if (userCheck == null) {
+            String userEmailInEntity = userEntity.getEmail();
+            UserEntity emailCheck = userDao.getUserByEmail(userEmailInEntity);
+            if (emailCheck == null) {
+                String password = userEntity.getPassword();
+                String[] encryptedText = passwordCryptographyProvider.encrypt(password);
+                userEntity.setSalt(encryptedText[0]);
+                userEntity.setPassword(encryptedText[1]);
+                userDao.createUser(userEntity);
+            }
+            else {
+                throw new SignUpRestrictedException("SGR-002",
+                        "This user has already been registered, try with any other emailId");
+            }
+        }
+        else {
+
+            throw new SignUpRestrictedException("SGR-001",
+                    "Try any other Username, this Username has already been taken");
+        }
+        return userEntity;
     }
 }
