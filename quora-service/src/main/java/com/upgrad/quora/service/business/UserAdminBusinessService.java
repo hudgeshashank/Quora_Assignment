f72f5780524083d543d1944977f5d3e49c0b1518
ChethanShetty563 <chethan563@gmail.com>
2020-12-06 22:52:51 +0530

    Added UserAdminBusinessService class which handles autoriztion for all the classes instead duplicating the code

---
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserAdminBusinessService.java
@@ -0,0 +1,27 @@
+package com.upgrad.quora.service.business;
+
+import com.upgrad.quora.service.dao.UserAuthDao;
+import com.upgrad.quora.service.entity.UserAuthTokenEntity;
+import com.upgrad.quora.service.exception.AuthorizationFailedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserAdminBusinessService {
+
+    @Autowired
+    private UserAuthDao userAuthDao;
+
+    public UserAuthTokenEntity authorize(final String authorization) throws AuthorizationFailedException {
+        UserAuthTokenEntity userAuthTokenEntity = userAuthDao.getAuthToken(authorization);
+
+        if (userAuthTokenEntity == null) {
+            throw new AuthorizationFailedException("ATHR-001", "User has not signed in");
+        } else if (userAuthTokenEntity.getLogoutAt() != null) {
+            throw new AuthorizationFailedException(
+                    "ATHR-002", "User is signed out.Sign in first to post a question");
+        }
+
+        return userAuthTokenEntity;
+    }
+}
